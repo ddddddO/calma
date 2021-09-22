8e84c6fc8c00a57824c99c7e7741b5c2690b31e1
ddddddO <lbfdeatq@gmail.com>
2021-09-23 03:31:26 +0900

    Add

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # calma
 Calendar for Markdown
+
+## Usage
+
+```sh
+03:21:44 > go run main.go
+|<font color="red">日</font>|月|火|水|木|金|<font color="blue">土</font>|
+|--|--|--|--|--|--|--|
+|<font color="red">22</font>|23|24|25|26|27|<font color="blue">28</font>|
+|<font color="red">29</font>|30|31|1|2|3|<font color="blue">4</font>|
+|<font color="red">5</font>|6|7|8|9|10|<font color="blue">11</font>|
+|<font color="red">12</font>|13|14|15|16|17|<font color="blue">18</font>|
+|<font color="red">19</font>|20|21|22|23|24|<font color="blue">25</font>|
+|<font color="red">26</font>|27|28|29|30|1|<font color="blue">2</font>|
+|<font color="red">3</font>|4|5|6|7|8|<font color="blue">9</font>|
+```
+
+Markdown
+
+|<font color="red">日</font>|月|火|水|木|金|<font color="blue">土</font>|
+|--|--|--|--|--|--|--|
+|<font color="red">22</font>|23|24|25|26|27|<font color="blue">28</font>|
+|<font color="red">29</font>|30|31|1|2|3|<font color="blue">4</font>|
+|<font color="red">5</font>|6|7|8|9|10|<font color="blue">11</font>|
+|<font color="red">12</font>|13|14|15|16|17|<font color="blue">18</font>|
+|<font color="red">19</font>|20|21|22|23|24|<font color="blue">25</font>|
+|<font color="red">26</font>|27|28|29|30|1|<font color="blue">2</font>|
+|<font color="red">3</font>|4|5|6|7|8|<font color="blue">9</font>|
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -20,22 +21,24 @@ type week struct {
 }
 
 func main() {
-	calendar, err := buildCalendar()
+	now := time.Now()
+
+	calendar, err := buildCalendar(now)
 	if err != nil {
 		fmt.Errorf("%+v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(calendar)
+	fmt.Print(calendar)
 }
 
 const (
 	calendarHeader = `|<font color="red">日</font>|月|火|水|木|金|<font color="blue">土</font>|`
 	calendarSplit  = `|--|--|--|--|--|--|--|`
-	weekTemplate   = `|{{.Sunday}}|{{.Monday}}|{{.Tuesday}}|{{.Wednesday}}|{{.Thursday}}|{{.Friday}}|{{.Saturday}}|`
+	weekTemplate   = `|<font color="red">{{.Sunday}}</font>|{{.Monday}}|{{.Tuesday}}|{{.Wednesday}}|{{.Thursday}}|{{.Friday}}|<font color="blue">{{.Saturday}}</font>|`
 )
 
-func buildCalendar() (string, error) {
+func buildCalendar(date time.Time) (string, error) {
 	buf := &strings.Builder{}
 	_, err := buf.WriteString(calendarHeader + "\n")
 	if err != nil {
@@ -46,14 +49,104 @@ func buildCalendar() (string, error) {
 		return "", errors.WithStack(err)
 	}
 
-	wk := week{Sunday: 1, Monday: 2, Tuesday: 3, Wednesday: 4, Thursday: 5, Friday: 6, Saturday: 7}
-	tmpl, err := template.New("week").Parse(weekTemplate)
-	if err != nil {
-		return "", errors.WithStack(err)
-	}
-	if err := tmpl.Execute(buf, wk); err != nil {
-		return "", errors.WithStack(err)
+	weeks := calculateWeeks(date)
+	for _, wk := range weeks {
+		tmpl, err := template.New("week").Parse(weekTemplate)
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
+		if err := tmpl.Execute(buf, wk); err != nil {
+			return "", errors.WithStack(err)
+		}
+		_, err = buf.WriteString("\n")
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
 	}
 
 	return buf.String(), nil
+}
+
+func calculateWeeks(date time.Time) []week {
+	current := date
+	wk := calculateWeek(current)
+	weeks := []week{wk}
+
+	retreat := current
+	// currentの前月の最終週まで遡る
+	for {
+		retreat = retreat.AddDate(0, 0, -7)
+		if retreat.Month() != current.Month() {
+			wk := calculateWeek(retreat)
+			weeks = append([]week{wk}, weeks...)
+			break
+		}
+		wk := calculateWeek(retreat)
+		weeks = append([]week{wk}, weeks...)
+	}
+
+	// currentの次月の初週まで進む
+	advance := current
+	for {
+		advance = advance.AddDate(0, 0, 7)
+		if advance.Month() != current.Month() {
+			wk := calculateWeek(advance)
+			weeks = append(weeks, wk)
+			break
+		}
+		wk := calculateWeek(advance)
+		weeks = append(weeks, wk)
+	}
+
+	return weeks
+}
+
+func calculateWeek(point time.Time) week {
+	wk := week{}
+	// pointの週の日曜日まで遡る
+	retreat := point
+	for {
+		if retreat.Weekday() == time.Sunday {
+			wk.Sunday = uint(retreat.Day())
+			break
+		}
+		switch retreat.Weekday() {
+		case time.Monday:
+			wk.Monday = uint(retreat.Day())
+		case time.Tuesday:
+			wk.Tuesday = uint(retreat.Day())
+		case time.Wednesday:
+			wk.Wednesday = uint(retreat.Day())
+		case time.Thursday:
+			wk.Thursday = uint(retreat.Day())
+		case time.Friday:
+			wk.Friday = uint(retreat.Day())
+		case time.Saturday:
+			wk.Saturday = uint(retreat.Day())
+		}
+		retreat = retreat.AddDate(0, 0, -1)
+	}
+
+	// pointの週の土曜日まで進む
+	advance := point
+	for {
+		if advance.Weekday() == time.Saturday {
+			wk.Saturday = uint(advance.Day())
+			break
+		}
+		switch advance.Weekday() {
+		case time.Monday:
+			wk.Monday = uint(advance.Day())
+		case time.Tuesday:
+			wk.Tuesday = uint(advance.Day())
+		case time.Wednesday:
+			wk.Wednesday = uint(advance.Day())
+		case time.Thursday:
+			wk.Thursday = uint(advance.Day())
+		case time.Friday:
+			wk.Friday = uint(advance.Day())
+		}
+		advance = advance.AddDate(0, 0, 1)
+	}
+	return wk
 }
